8d20f1809b042ffd507c7c077e2a95a3864ebb91
Samuel <knipknap@gmail.com>
2012-10-04 06:03:55 -0400

    the editor is now an object.

--- spiffform/spiffform.js
+++ spiffform/spiffform.js
@@ -401,48 +401,161 @@ var spiffform_elements = {
 // ======================================================================
 // Form Editor
 // ======================================================================
-// Returns True if the given (mouse pointer) position is within the given
-// distance around the given element.
-function spiffform_in_element(elem, x, y, distance) {
-    if (typeof distance === 'undefined')
-        distance = 0;
-    return x > elem.offset().left - distance &&
-           x < elem.offset().left + elem.width() + distance &&
-           y > elem.offset().top - distance &&
-           y < elem.offset().top + elem.height() + distance;
-}
+var SpiffFormEditor = function(form_div, panel) {
+    this._form = form_div;
+    this._panel = panel;
+    var that = this;
 
-// Initializes all divs that have the 'spiffform' class as forms.
-function spiffform_init(selector, panel) {
-    var name = 'Untitled';
-    var subtitle = 'Please fill out the form.';
+    if (this._form.length != 1)
+        throw new Error('form selector needs to match exactly one element');
+    this._panel.hide();
 
-    selector.each(function() {
-        $(this).data('panel', panel);
-        $(this).append('<div class="spiffform-canvas">' +
-                       '<ul class="spiffform-canvas-elements">' +
-                       '<li><h2 class="spiffform-title">' + name + '</h2></li>' +
-                       '<li><input type="text" class="spiffform-subtitle" name="subtitle" value="' + subtitle + '"/></li>' +
-                       '<li><hr/></li>' +
-                       '</ul>' +
-                       '<div class="spiffform-canvas-hint"><span></span></div>' +
-                       '</div>');
-        $(this).click(function() { spiffform_unselect($(this)); });
-    });
-    spiffform_set_hint(selector, 'drag');
+    // Returns True if the given (mouse pointer) position is within the given
+    // distance around the given element.
+    this._in_element = function(elem, x, y, distance) {
+        if (typeof distance === 'undefined')
+            distance = 0;
+        return x > elem.offset().left - distance &&
+               x < elem.offset().left + elem.width() + distance &&
+               y > elem.offset().top - distance &&
+               y < elem.offset().top + elem.height() + distance;
+    };
+    
+    // Change the name/title of the form.
+    this.set_name = function(name) {
+        this._form.find('.spiffform-title').text(name);
+    };
+    
+    // Change the subtitle of the form.
+    this.set_subtitle = function(subtitle) {
+        this._form.find('.spiffform-subtitle').val(subtitle);
+    };
+    
+    // Change the hint shown underneath of the form.
+    this.set_hint = function(hint) {
+        var elem = this._form.find('.spiffform-canvas-hint');
+        var span = elem.find('span');
+        elem.removeClass('spiffform-canvas-hint-arrow-right');
+        elem.removeClass('spiffform-canvas-hint-delete');
+        if (hint == 'drag') {
+            span.text('Drag form elements from the right');
+            elem.addClass('spiffform-canvas-hint-arrow-right');
+        }
+        else if (hint == 'delete') {
+            span.text('Drop here to remove');
+            elem.addClass('spiffform-canvas-hint-delete');
+        }
+        else {
+            span.text(hint);
+        }
+    };
 
+    // Unselect all items.
+    this.unselect = function(hide_panel) {
+        this._form.find('*').removeClass('spiffform-canvas-item-selected');
+        if (hide_panel || typeof hide_panel === 'undefined')
+            this._panel.hide('slow');
+    };
+
+    // Select the given item. Expects an li element.
+    this.select = function(elem) {
+        this.unselect(false);
+        elem.addClass('spiffform-canvas-item-selected');
+        this._panel.show_properties(elem.data('obj'), elem);
+    };
+
+    // Expects click events from li nodes.
+    this._element_clicked = function(event) {
+        that.select($(this));
+        return false;
+    };
+
+    // Returns dom for a new form element of the given type.
+    this._create_element = function(element_type) {
+        var obj = new spiffform_elements[element_type]();
+        var elem = $('<li class="spiffform-canvas-item">' +
+                     '<div class="spiffform-ui-element ' + element_type + '">' +
+                     '</div>' +
+                     '</li>');
+        obj.update_html(elem.find('div'));
+        elem.data('obj', obj);
+        elem.click(this._element_clicked);
+        obj.bind('changed', function() {
+            var div = elem.find('.spiffform-ui-element');
+            div.empty();
+            obj.update_html(div);
+        });
+        return elem;
+    };
+
+    // Inserts a new element at the end of the form.
+    this.append = function(element_type) {
+        var elem = this._create_element(element_type);
+        this._form.find('.spiffform-canvas-elements').append(elem);
+        return elem.data('obj');
+    };
+
+    // Removes the given element from the form. Expects an li element.
+    this.remove = function(elem) {
+        elem.remove();
+        this.unselect();
+    };
+
+    // Inserts a new element at the position that is specified within the given
+    // event.
+    this.insert_at = function(event, element_type) {
+        // Make sure that the element was dropped within this form.
+        var target = $(event.toElement);
+        var form = target.parents().andSelf().filter('div.spiffform').first();
+        if (!form.length)
+            return;
+
+        // Insert at the appropriate position, or append if this is the first item.
+        var elem = this._create_element(element_type);
+        if (!target.is('li'))
+            target = target.parents('li').first();
+        if (target.is('li.spiffform-canvas-item'))
+            elem.insertBefore(target);
+        else if (target.is('li')) {
+            // Dropped on the form header.
+            elem.insertAfter(this._form.find('li:not(.spiffform-canvas-item):last'));
+        }
+        else {
+            // Dropped on the form, but not on the element list.
+            this._form.find('.spiffform-canvas-elements').append(elem);
+        }
+    };
+
+    // Create the dom for the form.
+    this._form.data('panel', this._panel); // FIXME: get rid of
+    this._form.append('<div class="spiffform-canvas">' +
+                    '<ul class="spiffform-canvas-elements">' +
+                    '<li><h2 class="spiffform-title"></h2></li>' +
+                    '<li><input type="text" class="spiffform-subtitle" name="subtitle" value=""/></li>' +
+                    '<li><hr/></li>' +
+                    '</ul>' +
+                    '<div class="spiffform-canvas-hint"><span></span></div>' +
+                    '</div>');
+    this._form.click(function() { that.unselect($(this)); });
+    this.set_name('Untitled');
+    this.set_subtitle('Please fill out the form.');
+    this.set_hint('drag');
+
+    // Callback function that returns true if the given event happened within
+    // the form, returns false otherwise.
     function in_form(e, ui) {
         var form = $(e.toElement).parents('.spiffform').first();
-        return spiffform_in_element(form, e.pageX, e.pageY, 40);
+        return that._in_element(form, e.pageX, e.pageY, 40);
     }
 
-    selector.find('.spiffform-canvas-elements').sortable({
+    // Initialize the events on the dom.
+    this._form.find('.spiffform-canvas-elements').sortable({
         cancel: ':input:not([type=button])',
         items: 'li.spiffform-canvas-item',
         placeholder: 'spiffform-canvas-placeholder',
         distance: 3,
         axis: 'y',
-        containment: selector,
+        containment: this._form,
         forcePlaceholderSize: true,
         receive: function(e, ui) {
             ui.placeholder.toggle(true);
@@ -454,142 +567,36 @@ function spiffform_init(selector, panel) {
             ui.placeholder.toggle(false);
         },
         start: function(e, ui) {
-            var form = ui.item.parents('.spiffform').first();
-            spiffform_set_hint(form, 'delete');
+            that.set_hint('delete');
         },
         beforeStop: function(e, ui) {
-            var form = ui.item.parents('.spiffform').first();
-            spiffform_set_hint(form, 'drag');
+            that.set_hint('drag');
             if (!ui.placeholder.is(':visible'))
-                spiffform_remove(form, ui.item);
+                that.remove(ui.item);
         }
     });
-
-    panel.hide();
-}
-
-// Change the name/title of the form.
-function spiffform_set_name(form, name) {
-    form.find('.spiffform-title').text(name);
-}
-
-// Change the subtitle of the form.
-function spiffform_set_subtitle(form, subtitle) {
-    form.find('.spiffform-subtitle').val(subtitle);
-}
-
-// Change the hint shown underneath of the form.
-function spiffform_set_hint(form, hint) {
-    var elem = form.find('.spiffform-canvas-hint');
-    var span = elem.find('span');
-    elem.removeClass('spiffform-canvas-hint-arrow-right');
-    elem.removeClass('spiffform-canvas-hint-delete');
-    if (hint == 'drag') {
-        span.text('Drag form elements from the right');
-        elem.addClass('spiffform-canvas-hint-arrow-right');
-    }
-    else if (hint == 'delete') {
-        span.text('Drop here to remove');
-        elem.addClass('spiffform-canvas-hint-delete');
-    }
-    else {
-        span.text(hint);
-    }
-}
-
-// Unselect all items.
-function spiffform_unselect(form, hide_panel) {
-    form.find('*').removeClass('spiffform-canvas-item-selected');
-    form.data('selected', undefined);
-    if (hide_panel || typeof hide_panel === 'undefined')
-        spiffform_panel_show_properties(form);
-}
-
-// Select the given item. Expects an li element.
-function spiffform_select(form, elem) {
-    spiffform_unselect(form, false);
-    elem.addClass('spiffform-canvas-item-selected');
-    form.data('selected', elem);
-    spiffform_panel_show_properties(form, elem);
-}
-
-// Expects click events from li nodes.
-function spiffform_element_clicked(event) {
-    var form = $(this).parents('div.spiffform').first();
-    spiffform_select(form, $(this));
-    return false;
-}
-
-// Returns dom for a new form element of the given type.
-function spiffform_create_element(form, element_type) {
-    var obj = new spiffform_elements[element_type]();
-    var elem = $('<li class="spiffform-canvas-item">' +
-                 '<div class="spiffform-ui-element ' + element_type + '">' +
-                 '</div>' +
-                 '</li>');
-    obj.update_html(elem.find('div'));
-    elem.data('obj', obj);
-    elem.click(spiffform_element_clicked);
-    obj.bind('changed', function() {
-        var div = elem.find('.spiffform-ui-element');
-        div.empty();
-        obj.update_html(div);
-    });
-    return elem;
-}
-
-// Inserts a new element at the end of the form.
-function spiffform_append(form, element_type) {
-    var elem = spiffform_create_element(form, element_type);
-    form.find('.spiffform-canvas-elements').append(elem);
-    return elem.data('obj');
-}
-
-// Removes the given element from the form. Expects an li element.
-function spiffform_remove(form, elem) {
-    elem.remove();
-    spiffform_unselect(form);
-}
-
-// Inserts a new element at the position that is specified within the given
-// event.
-function spiffform_insert_at(event, element_type) {
-    // Make sure that the element was dropped within this form.
-    var target = $(event.toElement);
-    var form = target.parents().andSelf().filter('div.spiffform').first();
-    if (!form.length)
-        return;
-
-    // Insert at the appropriate position, or append if this is the first item.
-    var elem = spiffform_create_element(form, element_type);
-    if (!target.is('li'))
-        target = target.parents('li').first();
-    if (target.is('li.spiffform-canvas-item'))
-        elem.insertBefore(target);
-    else if (target.is('li')) {
-        // Dropped on the form header.
-        elem.insertAfter(form.find('li:not(.spiffform-canvas-item):last'));
-    }
-    else {
-        // Dropped on the form, but not on the element list.
-        form.find('.spiffform-canvas-elements').append(elem);
-    }
-}
+};
 
 // ======================================================================
 // Form Editor Panel (for showing properties, etc.)
 // ======================================================================
-// Show properties for the given item. Expects an li element.
-function spiffform_panel_show_properties(form, elem) {
-    var panel = form.data('panel');
-    if (typeof elem === 'undefined') {
-        panel.hide('slow');
-        return;
-    }
-    var obj = elem.data('obj');
-    panel.empty();
-    panel.append('<h3>' + obj._name + ' Properties</h3>' +
-                 '<div class="spiffform-panel-content"></div>');
-    obj.update_properties(panel.find('div'));
-    panel.show('fast');
-}
+var SpiffFormPanel = function(panel_div) {
+    this._panel = panel_div;
+
+    if (this._panel.length != 1)
+        throw new Error('panel selector needs to match exactly one element');
+
+    // Hide the panel.
+    this.hide = function(speed) {
+        this._panel.hide(speed);
+    };
+
+    // Show properties for the given item. Expects an li element.
+    this.show_properties = function(obj, elem) {
+        this._panel.empty();
+        this._panel.append('<h3>' + obj._name + ' Properties</h3>' +
+                           '<div class="spiffform-panel-content"></div>');
+        obj.update_properties(this._panel.find('div'));
+        this._panel.show('fast');
+    };
+};
