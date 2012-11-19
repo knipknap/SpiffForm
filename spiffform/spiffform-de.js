b463b6316604fa8eb81e5f000e8cc9379b1fa5a5
Josip Delic <delijati@googlemail.com>
2012-11-19 12:34:54 +0100

    Subtree merged in js/spiffform/resources/

---
+++ spiffform/spiffform-de.js
@@ -0,0 +1,31 @@
+var translation = {
+  'Drag form elements from the right': 'Ziehen Sie Elemente von der rechten Seite',
+  'Drop here to remove': 'Hier loslassen um zu löschen',
+  'Please correct the errors above.': 'Bitte korrigieren Sie die obigen Fehler.',
+  'Label': 'Beschriftung',
+  'Firstname/Lastname': 'Vorname/Nachname',
+  'Please send more spam to my inbox': 'Bestätigen Sie das Formular',
+  'Date': 'Datum',
+  'Please choose': 'Auswählen',
+  'Required field': 'Pflichtfeld',
+  'Default': 'Default',
+  'Options': 'Optionen',
+  'Properties': 'Eigenschaften',
+  'unnamed': 'Unbenannt',
+  'Title': 'Titel',
+  'Untitled': 'Ohne Titel',
+  'Subtitle': 'Untertitel',
+  'Please fill out the form': 'Fomular bitte ausfüllen',
+  'Separator': 'Trennzeichen',
+  'Name Field': 'Namensfeld',
+  'Entry Field': 'Eingabefeld',
+  'Text Area': 'Textfeld',
+  'Button': 'Schaltfläche',
+  'Checkbox': 'Ankreuzfeld',
+  'Date Picker': 'Datumsfeld',
+  'Dropdown List': 'Dropdownliste'
+};
+$(document).ready(function() {
+  $.i18n.setDictionary(translation);
+});
+
