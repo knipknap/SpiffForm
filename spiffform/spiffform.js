88fff43c69683a98005a80e5be40d379765e09bd
Josip Delic <delijati@googlemail.com>
2013-08-20 15:33:10 +0200

    -m "added range to date widget"

--- spiffform/spiffform.js
+++ spiffform/spiffform.js
@@ -923,7 +923,10 @@ var SpiffFormDatePicker = function() {
             onSelect: function(dateText, inst) {
                 that._value = $(this).datepicker('getDate');
                 that.validate();
-            }
+            },
+            changeMonth: true,
+            changeYear: true,
+            yearRange: "-100:+0"
         });
         picker.datepicker('setDate', that._value);
 
@@ -951,7 +954,10 @@ var SpiffFormDatePicker = function() {
             onSelect: function(dateText, inst) {
                 that._value = $(this).datepicker('getDate');
                 that.update();
-            }
+            },
+            changeMonth: true,
+            changeYear: true,
+            yearRange: "-100:+0"
         });
         picker.datepicker('setDate', that._value);
 
